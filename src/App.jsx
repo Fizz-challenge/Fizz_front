b1b114e49f1c84a290cf774c3b68e1c6751297fb
jsy0605 <jungsy0605@naver.com>
2024-07-20 20:47:18 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FollowPage from './pages/FollowPage';
 import SearchPage from './pages/SearchPage';
 
+//123
 const App = () => {
   return (
     <Router>
