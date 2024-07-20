e86cdabc471adf85038a3d74d149f300a0976f95
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-07-20 19:45:02 +0900

    [add] header 추가 및 그에 따른 전체 css 마진 조정

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './Components/Header'
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './Pages/MainPage/MainPage';
+import VideoDetail from './Pages/DetailVideoPage/VideoDetail';
 
 function App() {
 
@@ -10,9 +11,10 @@ function App() {
       <Header />
       <div className='content-wrapper'>
       <Routes>
-        <Route exact path="/" element={<MainPage />} />
-        </Routes>
-        </div>
+          <Route exact path="/" element={<MainPage />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+      </Routes>
+      </div>
 </>
   )
 }
