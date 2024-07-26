215c2021af790cb03e7d05cc46d9a9954806d84c
jsy0605 <jungsy0605@naver.com>
2024-07-26 20:50:47 +0900

    [add] 팔로우 삭제 기능 추가

--- src/Page/FollowPage/UserBlock.jsx
+++ src/Page/FollowPage/UserBlock.jsx
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Modal from 'react-modal';
 import './UserBlock.css';
 import humanIcon from '../../assets/human.png';
 
-const UserBlock = ({ userId, username, description, profileImage }) => {
+const UserBlock = ({ userId, username, description, profileImage, onFollowToggle }) => {
   const navigate = useNavigate();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const handleClick = () => {
     navigate(`/profile/${userId}`);
   };
 
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const confirmUnfollow = () => {
+    onFollowToggle();
+    closeModal();
+  };
+
   return (
-    <div className='userBlock' onClick={handleClick}>
-      <img src={profileImage || humanIcon} alt="User Icon" className='userIcon' />
-      <div className='userInfo'>
-        <div className='username'>{username}</div>
-        <div className='description'>{description}</div>
+    <div className='userBlock'>
+      <div className='userInfoContainer' onClick={handleClick}>
+        <img src={profileImage || humanIcon} alt="User Icon" className='userIcon' />
+        <div className='userInfo'>
+          <div className='username'>{username}</div>
+          <div className='description'>{description}</div>
+        </div>
       </div>
+      <button className='followToggleButton' onClick={(e) => { e.stopPropagation(); openModal(); }}>
+        팔로우 취소
+      </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Unfollow Confirmation"
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <h2>정말 취소하시겠습니까?</h2>
+        <div className="modal-buttons">
+          <button onClick={confirmUnfollow} className="confirm-button">네</button>
+          <button onClick={closeModal} className="cancel-button">아니오</button>
+        </div>
+      </Modal>
     </div>
   );
 };
