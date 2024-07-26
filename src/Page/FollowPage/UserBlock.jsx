3481ea817dff3c73a123f92263b0e7fad815aa2a
jsy0605 <jungsy0605@naver.com>
2024-07-26 18:01:00 +0900

    [docs] 유저 블럭 클릭 시 이동

--- src/Page/FollowPage/UserBlock.jsx
+++ src/Page/FollowPage/UserBlock.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './UserBlock.css';
 import humanIcon from '../../assets/human.png';
 
-const UserBlock = ({ username, description }) => {
+const UserBlock = ({ userId, username, description, profileImage }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/profile/${userId}`);
+  };
+
   return (
-    <div className='userBlock'>
-      <img src={humanIcon} alt="User Icon" className='userIcon' />
+    <div className='userBlock' onClick={handleClick}>
+      <img src={profileImage || humanIcon} alt="User Icon" className='userIcon' />
       <div className='userInfo'>
         <div className='username'>{username}</div>
         <div className='description'>{description}</div>
