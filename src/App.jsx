38127169db6bdf4e8eba2eaa45cff4bd20ea8c24
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-08-03 21:15:48 +0900

    [fix] App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./Components/Header";
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import MainPage from "./Page/MainPage/MainPage";
 import VideoDetail from "./Page/DetailVideoPage/VideoDetail";
 import FollowPage from "./Page/FollowPage/FollowPage";
@@ -12,25 +13,31 @@ import UserPage from "./Page/UserPage/UserPage.jsx";
 import NewPost from './Page/NewPostPage/NewPost.jsx';
 import ChallengePage from './Page/ChallengePage/ChallengePage.jsx';
 import OAuth2Callback from "./Page/LoginPage/OAuth2Callback.jsx";
+import DeleteProfilePage from "./Page/DeleteProfilePage/DeleteProfilePage.jsx";
 import NewChallengePage from './Page/NewChallengePage/NewChallengePage.jsx';
 
 function App() {
+	const navigate = useNavigate();
+	useEffect(() => {
+		
+	}, [navigate])
 	return (
 		<>
-			<Header />
-			<div className="content-wrapper">
+			{location.pathname !== "/login" && location.pathname !== "/register" && (<Header />)}
+			<div className={`content-wrapper ${(location.pathname === "/login" || location.pathname === "/register") ? "noHeader" : ""}`}>
 				<Routes>
 					<Route exact path="/" element={<MainPage />} />
 					<Route path="/video/:id" element={<VideoDetail />} />
 					<Route path="/follow" element={<FollowPage />} />
 					<Route path="/search" element={<SearchPage />} />
-					<Route path="/login" element={<LoginPage />} />
-					<Route path="/register" element={<RegisterPage />} />
-          <Route path="/profile/:userId" element={<UserPage />} />
+               		<Route path="/profile/:userId" element={<UserPage />} />
 					<Route path="/new-post" element={<NewPost />} />
 					<Route path="/challenge/:challenge" element={<ChallengePage />} />
-					<Route path="/oauth2/callback" element={<OAuth2Callback />} />
+               		<Route path="/oauth2/callback" element={<OAuth2Callback />} />
 					<Route path="/category/:categoryName" element={<CategoryPage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/register" element={<RegisterPage />} />
+					<Route path="/delete-profile" element={<DeleteProfilePage />} />
 					<Route path="/:id" element={<MainPage />} />
 					<Route path="/new-challenge" element={<NewChallengePage />} />
 				</Routes>
