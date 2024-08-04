79b505f9723a41482c1e86dfbcfb531326090197
jsy0605 <jungsy0605@naver.com>
2024-08-04 22:59:02 +0900

    [docs] SearchBar 오류 해결

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -2,45 +2,38 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './SearchBar.css';
 
-const SearchBar = ({ onSearch, onFocus, onBlur, className }) => {
-  const [searchTerm, setSearchTerm] = useState('');
+const SearchBar = ({ className = '', onSearch, onFocus = () => {}, onBlur = () => {} }) => {
+  const [term, setTerm] = useState('');
 
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
+  const handleChange = (e) => {
+    setTerm(e.target.value);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onSearch(searchTerm);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(term);
   };
 
   return (
     <form className={`search-bar ${className}`} onSubmit={handleSubmit}>
       <input
         type="text"
-        value={searchTerm}
+        value={term}
         onChange={handleChange}
         onFocus={onFocus}
         onBlur={onBlur}
-        placeholder="검색어를 입력하세요"
-        className="search-input"
+        placeholder="검색어를 입력하세요..."
       />
-      <button type="submit" className="search-button">검색</button>
+      <button type="submit">검색</button>
     </form>
   );
 };
 
 SearchBar.propTypes = {
+  className: PropTypes.string,
   onSearch: PropTypes.func.isRequired,
   onFocus: PropTypes.func,
   onBlur: PropTypes.func,
-  className: PropTypes.string,
-};
-
-SearchBar.defaultProps = {
-  onFocus: () => {},
-  onBlur: () => {},
-  className: '',
 };
 
 export default SearchBar;
