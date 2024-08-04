439a8da7139fc766fd8bfcd427bcd3cada4836f0
jsy0605 <jungsy0605@naver.com>
2024-08-04 21:59:54 +0900

    [docs] 카테고리 썸네일 오류 수정

--- src/Page/SearchPage/CategoryThumbnail.jsx
+++ src/Page/SearchPage/CategoryThumbnail.jsx
@@ -7,14 +7,16 @@ const CategoryThumbnail = ({ category }) => {
   const navigate = useNavigate();
 
   const handleClick = () => {
-    navigate(`/category/${category.name}`);
+    navigate(`/category/${category.categoryId}/${category.title}`);
   };
 
   return (
     <div className="category-thumbnail" onClick={handleClick}>
-      <div className="category-thumbnail-skeleton"></div>
+      <div className="category-thumbnail-skeleton">
+        <img src={`/img/${category.image}`} alt={category.title} />
+      </div>
       <div className="category-info">
-        <h3 className="category-title">{category.name}</h3>
+        <h3 className="category-title">{category.title}</h3>
         <p className="category-description">{category.description}</p>
       </div>
     </div>
@@ -23,7 +25,8 @@ const CategoryThumbnail = ({ category }) => {
 
 CategoryThumbnail.propTypes = {
   category: PropTypes.shape({
-    name: PropTypes.string.isRequired,
+    categoryId: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
   }).isRequired,
