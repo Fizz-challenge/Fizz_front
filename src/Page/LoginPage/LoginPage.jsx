bd7e73accfab39b445f9c8d817b20df9b8f6548d
chamd <chana051226@gmail.com>
2024-08-06 01:06:10 +0900

    [fix] 찐막 경로수정

--- src/Page/LoginPage/LoginPage.jsx
+++ src/Page/LoginPage/LoginPage.jsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
 			<div className="loginBack"></div>
 			<div className="loginAllWrap">
 				<div className="loginBackWrap">
-					<img src="../src/assets/fizz2.png" alt="로고" className="loginLogo" />
+					<img src="../img/fizz2.png" alt="로고" className="loginLogo" />
 				</div>
 				<div className="loginWrap">
 					<div className="loginTitle">환영합니다</div>
