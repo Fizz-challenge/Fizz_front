44875585633a9ad6492d3d6e687307af4028da0e
jsy0605 <jungsy0605@naver.com>
2024-08-05 22:04:49 +0900

    [docs] 검색어 기반 게시글 찾기 오류 수정

--- src/Page/SearchPage/PostSearchResults.jsx
+++ src/Page/SearchPage/PostSearchResults.jsx
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IoPlay } from "react-icons/io5";
+import { FaComment, FaHeart } from "react-icons/fa6";
+import PostText from "../UserPage/PostText.jsx";
+import "./PostSearchResults.css";
 
-const PostSearchResults = ({ filteredPosts = [] }) => {
-  return filteredPosts.length > 0 ? (
-    <div className="posts-container">
-      {filteredPosts.map((post) => (
-        <div key={post.id} className="post-card">
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
-          <p>{`작성자: ${post.userInfo.nickname}`}</p>
-          <p>{`조회수: ${post.viewCount} 댓글수: ${post.commentCount} 좋아요: ${post.likeCount}`}</p>
-          <p>{`챌린지: ${post.challengeInfo.title}`}</p>
-        </div>
-      ))}
+const PostSearchResults = ({ filteredPosts }) => {
+  const navigate = useNavigate();
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const handlePostHover = (id) => {
+    setSelectedPost(id);
+  };
+
+  const getThumbnail = (post) => {
+    return post.fileType === "VIDEO" ? post.fileUrls[1] : post.fileUrls[0];
+  };
+
+  return (
+    <div className="searchPosts-container">
+      {filteredPosts.length > 0 ? (
+        filteredPosts.map((post) => (
+          <div key={post.id} className="searchPost">
+            <img className="searchPostThumbnail" src={getThumbnail(post)} alt="썸네일" />
+            <div className="searchPostViewCount">
+              <IoPlay className="searchPostIcon" />
+              {post.viewCount}
+            </div>
+            <div
+              className="searchPostHover"
+              onClick={() => navigate(`/video/${post.id}`)}
+              onMouseEnter={() => handlePostHover(post.id)}
+            >
+              <div className="searchPostLikeCount">
+                <FaHeart className="searchPostIcon" />
+                {post.likeCount}
+              </div>
+              <div className="searchPostCommentCount">
+                <FaComment className="searchPostIcon" />
+                {post.commentCount}
+              </div>
+            </div>
+            <PostText text={post.title} type="title" />
+            <PostText text={`#${post.challengeInfo.title}`} type="challenge" />
+          </div>
+        ))
+      ) : (
+        <p>게시글을 찾을 수 없습니다.</p>
+      )}
     </div>
-  ) : (
-    <p>게시글을 찾을 수 없습니다.</p>
   );
 };
 
