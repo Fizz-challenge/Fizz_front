e8ad8b433f00406af492330abdf14da3f9190754
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-08-05 13:22:44 +0900

    [add] 디테일 추가

---
+++ src/Page/DetailVideoPage/utils.jsx
@@ -0,0 +1,33 @@
+export function timeSince(date) {
+  const adjustedDate = new Date(date);
+  adjustedDate.setHours(adjustedDate.getHours() + 9);
+
+  const seconds = Math.floor((new Date() - adjustedDate) / 1000);
+
+  let interval = Math.floor(seconds / 31536000);
+  if (interval >= 1) {
+    return `${interval}년 전`;
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval >= 1) {
+    return `${interval}달 전`;
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval >= 1) {
+    return `${interval}일 전`;
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval >= 1) {
+    return `${interval}시간 전`;
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval >= 1) {
+    return `${interval}분 전`;
+  }
+  if (seconds < 10) {
+    return "방금 전";
+  }
+  return `${Math.floor(seconds)}초 전`;
+}
+
+export default timeSince;
