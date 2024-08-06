c588be35b1f433700ec1604306173df0f0133e41
jsy0605 <jungsy0605@naver.com>
2024-08-06 23:27:31 +0900

    [docs] 게시글 스켈레톤 수정

--- src/Page/SearchPage/PostSearchResults.jsx
+++ src/Page/SearchPage/PostSearchResults.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { IoPlay } from "react-icons/io5";
 import { FaComment, FaHeart } from "react-icons/fa6";
@@ -9,6 +9,12 @@ import styles from "./PostSearchResults.module.css";
 const PostSearchResults = ({ filteredPosts, loading }) => {
   const navigate = useNavigate();
   const [selectedPost, setSelectedPost] = useState(null);
+  const [showSkeleton, setShowSkeleton] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSkeleton(false), 300);
+    return () => clearTimeout(timer);
+  }, []);
 
   const handlePostHover = (id) => {
     setSelectedPost(id);
@@ -18,12 +24,19 @@ const PostSearchResults = ({ filteredPosts, loading }) => {
     return post.fileType === "VIDEO" ? post.fileUrls[1] : post.fileUrls[0];
   };
 
-  if (loading) {
+  if (loading || showSkeleton) {
     return (
       <div className={styles.postsContainer}>
-        {Array.from({ length: 9 }).map((_, index) => (
+        {Array.from({ length: 6 }).map((_, index) => (
           <div key={index} className={styles.skeletonCard}>
-            <Skeleton width={250} height={380} borderRadius={15} />
+            <Skeleton width={250} height={320} borderRadius={15} />
+            <div className={styles.skeletonProfile}>
+              <Skeleton circle={true} width={40} height={40} />
+              <div className={styles.skeletonText}>
+                <Skeleton width={170} height={20} />
+                <Skeleton width={100} height={20} />
+              </div>
+            </div>
           </div>
         ))}
       </div>
--- src/Page/SearchPage/SearchPage.jsx
+++ src/Page/SearchPage/SearchPage.jsx
@@ -1,208 +1,224 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
-import './SearchPage.css';
-import CategoryThumbnail from './CategoryThumbnail';
-import SlideBar from '../../Components/SlideBar';
-import categoryData from './Category.json';
-import ChallengeSearchResults from './ChallengeSearchResults';
-import UserSearchResults from './UserSearchResults';
-import PostSearchResults from './PostSearchResults';
+  import React, { useState, useEffect, useCallback } from 'react';
+  import { useNavigate, useLocation } from 'react-router-dom';
+  import Skeleton from 'react-loading-skeleton';
+  import 'react-loading-skeleton/dist/skeleton.css';
+  import './SearchPage.css';
+  import CategoryThumbnail from './CategoryThumbnail';
+  import SlideBar from '../../Components/SlideBar';
+  import categoryData from './Category.json';
+  import ChallengeSearchResults from './ChallengeSearchResults';
+  import UserSearchResults from './UserSearchResults';
+  import PostSearchResults from './PostSearchResults';
 
-const SearchPage = ({ handleScroll, isScroll }) => {
-  const [loading, setLoading] = useState(true);
-  const [nowSelected, setNowSelected] = useState(0);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [showSlideBar, setShowSlideBar] = useState(false);
-  const [filteredChallenges, setFilteredChallenges] = useState([]);
-  const [filteredUsers, setFilteredUsers] = useState([]);
-  const [filteredPosts, setFilteredPosts] = useState([]);
-  const [followingUsers, setFollowingUsers] = useState([]);
+  const SearchPage = ({ handleScroll, isScroll }) => {
+    const [loading, setLoading] = useState(true);
+    const [nowSelected, setNowSelected] = useState(0);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [showSlideBar, setShowSlideBar] = useState(false);
+    const [filteredChallenges, setFilteredChallenges] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState([]);
+    const [filteredPosts, setFilteredPosts] = useState([]);
+    const [followingUsers, setFollowingUsers] = useState([]);
 
-  const navigate = useNavigate();
-  const location = useLocation();
+    const navigate = useNavigate();
+    const location = useLocation();
 
-  const fetchFollowingUsers = useCallback(async (token) => {
-    try {
-      const response = await fetch('https://gunwoo.store/api/user/me', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+    const fetchFollowingUsers = useCallback(async (token) => {
+      try {
+        const response = await fetch('https://gunwoo.store/api/user/me', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        if (result.success) {
+          setFollowingUsers(result.data.following);
+        }
+      } catch (error) {
+        console.error('Error fetching following users:', error);
       }
-      const result = await response.json();
-      if (result.success) {
-        setFollowingUsers(result.data.following);
+    }, []);
+
+    useEffect(() => {
+      const token = localStorage.getItem('accessToken');
+      fetchFollowingUsers(token);
+      setLoading(false);
+    }, [fetchFollowingUsers, navigate]);
+
+    useEffect(() => {
+      const params = new URLSearchParams(location.search);
+      const term = params.get('term');
+      if (term) {
+        setSearchTerm(term);
+        setShowSlideBar(true);
+
+        const fetchChallenges = async () => {
+          try {
+            const token = localStorage.getItem('accessToken');
+
+            const response = await fetch(`https://gunwoo.store/api/challenge/search?keyword=${term}`, {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            });
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            const result = await response.json();
+            if (result.success) {
+              setFilteredChallenges(result.data);
+            }
+          } catch (error) {
+            console.error('Error fetching challenges:', error);
+          }
+        };
+
+        const fetchUsers = async () => {
+          try {
+            const token = localStorage.getItem('accessToken');
+
+            const response = await fetch(`https://gunwoo.store/api/user/search?nickname=${term}`, {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            });
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            const result = await response.json();
+            if (result.success) {
+              console.log('Fetched Users:', result.data);
+              const currentUserProfileId = localStorage.getItem('profileId');
+              const filtered = result.data
+                .filter(user => user.profileId !== currentUserProfileId)
+                .map(user => ({
+                  ...user,
+                  isFollowing: followingUsers.some(followingUser => followingUser.profileId === user.profileId),
+                }));
+              setFilteredUsers(filtered);
+            } else {
+              console.log('No users found or result.success is false');
+            }
+          } catch (error) {
+            console.error('Error fetching users:', error);
+          }
+        };
+
+        const fetchPosts = async () => {
+          try {
+            const token = localStorage.getItem('accessToken');
+
+            const response = await fetch(`https://gunwoo.store/api/posts/search?keyword=${term}`, {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            });
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            const result = await response.json();
+            if (result.success) {
+              console.log('Fetched posts:', result.data.content);
+              setFilteredPosts(result.data.content);
+            }
+          } catch (error) {
+            console.error('Error fetching posts:', error);
+          }
+        };
+
+        fetchChallenges();
+        fetchUsers();
+        fetchPosts();
+      } else {
+        setShowSlideBar(false);
+        setSearchTerm('');
+        setFilteredChallenges([]);
+        setFilteredUsers([]);
+        setFilteredPosts([]);
       }
-    } catch (error) {
-      console.error('Error fetching following users:', error);
-    }
-  }, []);
+    }, [location.search, followingUsers, navigate]);
 
-  useEffect(() => {
-    const token = localStorage.getItem('accessToken');
-    fetchFollowingUsers(token);
-    setLoading(false);
-  }, [fetchFollowingUsers, navigate]);
+    const handleSearch = (term) => {
+      if (term) {
+        navigate(`/search?term=${term}`);
+      } else {
+        navigate('/search');
+      }
+    };
 
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const term = params.get('term');
-    if (term) {
-      setSearchTerm(term);
-      setShowSlideBar(true);
+    const navItems = [
+      { text: "게시글" },
+      { text: "사용자" },
+      { text: "챌린지" },
+    ];
 
-      const fetchChallenges = async () => {
-        try {
-          const token = localStorage.getItem('accessToken');
-
-          const response = await fetch(`https://gunwoo.store/api/challenge/search?keyword=${term}`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          const result = await response.json();
-          if (result.success) {
-            setFilteredChallenges(result.data);
-          }
-        } catch (error) {
-          console.error('Error fetching challenges:', error);
-        }
-      };
-
-      const fetchUsers = async () => {
-        try {
-          const token = localStorage.getItem('accessToken');
-
-          const response = await fetch(`https://gunwoo.store/api/user/search?nickname=${term}`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          const result = await response.json();
-          if (result.success) {
-            console.log('Fetched Users:', result.data);
-            const currentUserProfileId = localStorage.getItem('profileId');
-            const filtered = result.data
-              .filter(user => user.profileId !== currentUserProfileId)
-              .map(user => ({
-                ...user,
-                isFollowing: followingUsers.some(followingUser => followingUser.profileId === user.profileId),
-              }));
-            setFilteredUsers(filtered);
-          } else {
-            console.log('No users found or result.success is false');
-          }
-        } catch (error) {
-          console.error('Error fetching users:', error);
-        }
-      };
-
-      const fetchPosts = async () => {
-        try {
-          const token = localStorage.getItem('accessToken');
-
-          const response = await fetch(`https://gunwoo.store/api/posts/search?keyword=${term}`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          const result = await response.json();
-          if (result.success) {
-            console.log('Fetched posts:', result.data.content);
-            setFilteredPosts(result.data.content);
-          }
-        } catch (error) {
-          console.error('Error fetching posts:', error);
-        }
-      };
-
-      fetchChallenges();
-      fetchUsers();
-      fetchPosts();
-    } else {
-      setShowSlideBar(false);
-      setSearchTerm('');
-      setFilteredChallenges([]);
-      setFilteredUsers([]);
-      setFilteredPosts([]);
-    }
-  }, [location.search, followingUsers, navigate]);
-
-  const handleSearch = (term) => {
-    if (term) {
-      navigate(`/search?term=${term}`);
-    } else {
-      navigate('/search');
-    }
-  };
-
-  const navItems = [
-    { text: "게시글" },
-    { text: "사용자" },
-    { text: "챌린지" },
-  ];
-
-  const renderContent = () => {
-    if (nowSelected === 0) {
-      return <PostSearchResults filteredPosts={filteredPosts} loading={loading} />;
-    } else if (nowSelected === 1) {
-      return <UserSearchResults filteredUsers={filteredUsers} loading={loading} />;
-    } else if (nowSelected === 2) {
-      return <ChallengeSearchResults filteredChallenges={filteredChallenges} loading={loading} />;
-    }
-  };
-
-
-
-
-  return (
-    <div className="search-page-container">
-      <div className="SlideBarWrapper">
-        {showSlideBar && (
-          <SlideBar nowSelected={nowSelected} setNowSelected={setNowSelected} items={navItems} isScroll={isScroll} />
-        )}
-      </div>
-      <div className="search-page-main-content">
-        <div className="category-bar"></div>
-        {!showSlideBar && (
-          <div className="categories-grid">
-            {loading ? (
-              <>
-                {Array.from({ length: 6 }).map((_, index) => (
-                  <div key={index} className="category-thumbnail">
-                    <Skeleton height={150} />
-                    <Skeleton height={20} width={100} />
+    const renderContent = () => {
+      if (loading) {
+        return (
+          <div className={styles.postsContainer}>
+            {Array.from({ length: 6 }).map((_, index) => (
+              <div key={index} className={styles.skeletonCard}>
+                <Skeleton width={250} height={320} borderRadius={15} />
+                <div className={styles.skeletonProfile}>
+                  <Skeleton circle={true} width={40} height={40} />
+                  <div className={styles.skeletonText}>
+                    <Skeleton width={170} height={20} />
+                    <Skeleton width={100} height={20} />
                   </div>
-                ))}
-              </>
-            ) : (
-              categoryData.data.map((category, index) => (
-                <CategoryThumbnail key={index} category={category} />
-              ))
-            )}
+                </div>
+              </div>
+            ))}
           </div>
-        )}
-        {showSlideBar && (
-          <div className="search-page-content">
-            {renderContent()}
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
+        );
+      }
 
-export default SearchPage;
+      if (nowSelected === 0) {
+        return <PostSearchResults filteredPosts={filteredPosts} loading={loading} />;
+      } else if (nowSelected === 1) {
+        return <UserSearchResults filteredUsers={filteredUsers} loading={loading} />;
+      } else if (nowSelected === 2) {
+        return <ChallengeSearchResults filteredChallenges={filteredChallenges} loading={loading} />;
+      }
+    };
+
+    return (
+      <div className="search-page-container">
+        <div className="SlideBarWrapper">
+          {showSlideBar && (
+            <SlideBar nowSelected={nowSelected} setNowSelected={setNowSelected} items={navItems} isScroll={isScroll} />
+          )}
+        </div>
+        <div className="search-page-main-content">
+          <div className="category-bar"></div>
+          {!showSlideBar && (
+            <div className="categories-grid">
+              {loading ? (
+                <>
+                  {Array.from({ length: 6 }).map((_, index) => (
+                    <div key={index} className="category-thumbnail">
+                      <Skeleton height={150} />
+                      <Skeleton height={20} width={100} />
+                    </div>
+                  ))}
+                </>
+              ) : (
+                categoryData.data.map((category, index) => (
+                  <CategoryThumbnail key={index} category={category} />
+                ))
+              )}
+            </div>
+          )}
+          {showSlideBar && (
+            <div className="search-page-content">
+              {renderContent()}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  };
+
+  export default SearchPage;
